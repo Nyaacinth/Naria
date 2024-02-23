fba3a934ec919164fdb3e810f19a98d8046b3776
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2024-02-23 21:00:05 +0800

    Remove useEffect hook

--- app/hooks/useRandomBoolean.ts
+++ app/hooks/useRandomBoolean.ts
@@ -1,7 +1,5 @@
 import { useEffect, useReducer } from "react"
 
 export function useRandomBoolean(): [randomBool: boolean, next: () => void] {
-    const [randomBool, next] = useReducer(() => Math.random() > 0.5, false)
-    useEffect(() => next(), [])
-    return [randomBool, next]
+    return useReducer(() => Math.random() > 0.5, Math.random() > 0.5)
 }
