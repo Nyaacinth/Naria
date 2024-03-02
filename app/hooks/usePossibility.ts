476b1ee0f08dcd183f3cb8a204a1bf98467eea99
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2024-03-02 18:22:12 +0800

    Little Fix: remove non-reassigned var

--- app/hooks/usePossibility.ts
+++ app/hooks/usePossibility.ts
@@ -2,11 +2,10 @@ import { useReducer } from "react"
 
 export function usePossibility(type: "normal" | "percentage" = "normal"): [possibility: number, next: () => void] {
     const [possibility, next] = useReducer(() => Math.random(), Math.random())
-    const possibilityPercentage = Math.round(possibility * 100)
     switch (type) {
         case "normal":
             return [possibility, next]
         case "percentage":
-            return [possibilityPercentage, next]
+            return [Math.round(possibility * 100), next]
     }
 }
